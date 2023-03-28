0666dfa56ee6c1b52e78fdc3bdffca2bf47928a6
vandaele.joshua <folfyblu@gmail.com>
2023-03-28 17:16:38 +0200

    IA Optimize

--- IA_Proba.py
+++ IA_Proba.py
@@ -47,8 +47,7 @@ def is_one_pair(cards: list[Card]) -> int | None:
             pair = tuple_card[0]
             del occurence[occurence.index(tuple_card)]
 
-    if pair != 0: return pair
-    return None
+    return pair if pair != 0 else None
         
 def is_two_pairs(cards: list[Card]) -> tuple[int, int] | None:
 
@@ -64,31 +63,29 @@ def is_two_pairs(cards: list[Card]) -> tuple[int, int] | None:
 
     for value in to_del: occurence[occurence.index(value)]
 
-    if len(pair) >= 2: return pair[0], pair[1]
-    return None
+    return (pair[0], pair[1]) if len(pair) >= 2 else None
         
 def is_three_of_kind(cards: list[Card]) -> int | None:
 
     occurence: list[tuple[int, int]] = Card.sort_occurence(cards)
     three_of_kind: int = 0
-    
-    
+
+
     for tuple_card in reversed(occurence):
         if tuple_card[1] == 3 and (three_of_kind == 0 or (tuple_card[0] > three_of_kind or tuple_card[0] == 1)):
             three_of_kind = tuple_card[0]
 
-    if three_of_kind != 0: return three_of_kind
-    return None
+    return three_of_kind if three_of_kind != 0 else None
 
 def is_straight(cards: list[Card]) -> int | None:
                 
     cards.sort(reverse = True)
-    
+
     if 1 in cards and 13 in cards and 12 in cards and 11 in cards and 10 in cards: return 1
-    
+
     compteur: int = 0
     highest_value: int = 0
-    
+
     for i, card in enumerate(cards):
         if i < len(cards) - 1:
             if card - 1 == cards[i+1]:
@@ -98,68 +95,63 @@ def is_straight(cards: list[Card]) -> int | None:
             else:
                 compteur = 0
                 highest_value = 0
-                
-    if compteur >= 4: return highest_value
-    return None
+
+    return highest_value if compteur >= 4 else None
     
 def is_flush(cards: list[Card]) -> tuple[int] | None:
             
     cards.sort(reverse=True)
-    
+
     while cards[-1].value == 1 and sum(card.value for card in cards) != len(cards):
         cards.insert(0, cards[-1])
         del cards[-1]
-    
+
     suit: list[str] = []
     value: list[list[int]] = []
-    
+
     for card in cards:
         if card.suit not in suit:
             suit.append(card.suit)
             value.append([card.value])
         else:
             value[suit.index(card.suit)].append(card.value)
-            
-    for i, v in enumerate(value):
-        if len(v) >= 5: return tuple(card_value for card_value in v[:5])
-    return None
+
+    return next((tuple(v[:5]) for v in value if len(v) >= 5), None)
 
 def is_four_of_kind(cards: list[Card]) -> int | None:
     
     occurence: list[tuple[int, int]] = Card.sort_occurence(cards)
-    
+
     four_of_kind: int = 0
-    
+
     for tuple_card in occurence:
         if tuple_card[1] == 4:
             four_of_kind = tuple_card[0]
             del occurence[occurence.index(tuple_card)]
-            
-    if four_of_kind != 0: return four_of_kind
-    return None
+
+    return four_of_kind if four_of_kind != 0 else None
     
 
 def is_full_house(cards: list[Card]) -> tuple[int, int] | None:
 
     occurence:  list[tuple[int, int]] = Card.sort_occurence(cards)
-    
+
     three_of_kind: int = 0
     pair: int = 0
-    
+
     for tuple_card in occurence:
         if tuple_card[1] == 3 and (three_of_kind == 0 or tuple_card[0] > three_of_kind):
             three_of_kind = tuple_card[0]
         elif tuple_card[1] == 2 and (pair == 0 or tuple_card[0] > pair):
             pair = tuple_card[0]
-            
-    if three_of_kind != 0 and pair != 0: return three_of_kind, pair
-    return None
+
+    return (three_of_kind, pair) if three_of_kind != 0 and pair != 0 else None
 
 def is_straight_flush(cards: list[Card]) -> tuple[int, str] | None:
 
     card_value: list[list[int]] = []
     card_suit: list[str] = []
-    
+
     for card in sorted(cards, reverse = True):
         if card.suit not in card_suit:
             card_suit.append(card.suit)
@@ -175,127 +167,124 @@ def is_straight_flush(cards: list[Card]) -> tuple[int, str] | None:
             for i, card in enumerate(value):
                 if 1 in value and 13 in value and 12 in value and 11 in value and 10 in value:
                     return 1, card_suit[j]
-                
-                else:    
-                    if i < len(value) - 1:
-                        if card - 1 == value[i+1]: compteur += 1
-                        if highest_value[0] == 0: highest_value = (card, card_suit[j])
-                            
-                    elif compteur < 4:
-                        compteur = 0
-                        highest_value = (0, "")
-            
+
+                if i < len(value) - 1:
+                    if card - 1 == value[i+1]: compteur += 1
+                    if highest_value[0] == 0: highest_value = (card, card_suit[j])
+
+                elif compteur < 4:
+                    compteur = 0
+                    highest_value = (0, "")
+
             if compteur >= 4:
                 return highest_value
-        
+
     return None
 
 def is_royal_straight_flush(cards: list[Card]) -> str | None:
-        card_suit: list[str] = []
-        card_value: list[list[int]] = []
+    card_suit: list[str] = []
+    card_value: list[list[int]] = []
 
-        for card in sorted(cards, reverse=True):
-            if card.suit not in card_suit:
-                card_suit.append(card.suit)
-                card_value.append([card.value])
-            else:
-                card_value[card_suit.index(card.suit)].append(card.value)
+    for card in sorted(cards, reverse=True):
+        if card.suit not in card_suit:
+            card_suit.append(card.suit)
+            card_value.append([card.value])
+        else:
+            card_value[card_suit.index(card.suit)].append(card.value)
 
-        for i, value in enumerate(card_value):
-            if 1 in value and 13 in value and 12 in value and 11 in value and 10 in value:
-                return card_suit[i]
-
-        return None
+    return next(
+        (
+            card_suit[i]
+            for i, value in enumerate(card_value)
+            if 1 in value
+            and 13 in value
+            and 12 in value
+            and 11 in value
+            and 10 in value
+        ),
+        None,
+    )
     
 def complet_hand(current_hand: list, cards: list[Card]) -> list:
     
     # remove card use in hand
-    for card in reversed(cards[:len(cards)]):
+    for card in reversed(cards[:]):
         if card.value in current_hand:
             del cards[cards.index(card)]
-    
-    # complete the hand with most powerfull cards      
-    for i in range(len(cards)):
+
+    # complete the hand with most powerfull cards
+    for _ in range(len(cards)):
         highest_card: int = is_highest_card(cards)
         current_hand.append(highest_card)
-        
+
         for card in cards:
             if card.value == highest_card:
                 del cards[cards.index(card)]
                 break
-        
+
     # remove card useless in hand
     if current_hand[0] == 8: max_card: int = 2
-    elif current_hand[0] == 3: max_card: int = 3
-    elif current_hand[0] == 4: max_card: int = 3
+    elif current_hand[0] in [3, 4]: max_card: int = 3
     elif current_hand[0] == 2: max_card: int = 4
     else: max_card: int = 5
-    
+
     while len(current_hand[1:]) > max_card:
         del current_hand[-1]
-    
+
     return current_hand
     
 def get_best_combination(cards: list[Card]) -> list:
 
-        if is_royal_straight_flush(cards) != None:
-            return [10] 
+    if is_royal_straight_flush(cards) != None:
+        return [10] 
 
-        elif (straight_flush := is_straight_flush(cards)) != None:
-            return [9, straight_flush[0]]
+    elif (straight_flush := is_straight_flush(cards)) != None:
+        return [9, straight_flush[0]]
 
-        elif (four_of_kind := is_four_of_kind(cards)) != None:
-            return complet_hand([8, four_of_kind], cards)
-        
-        elif (full_house := is_full_house(cards)) != None:
-            return [7, full_house[0], full_house[1]]
+    elif (four_of_kind := is_four_of_kind(cards)) != None:
+        return complet_hand([8, four_of_kind], cards)
 
-        elif (flush := is_flush(cards)) != None:
-            return [6] + [value for value in flush]
+    elif (full_house := is_full_house(cards)) != None:
+        return [7, full_house[0], full_house[1]]
 
-        elif (straight := is_straight(cards)) != None:
-            return [5, straight]
+    elif (flush := is_flush(cards)) != None:
+        return [6] + list(flush)
 
-        elif (three_of_kind := is_three_of_kind(cards)) != None:
-            return complet_hand([4, three_of_kind], cards)
-        
-        elif (two_pairs := is_two_pairs(cards)) != None:
-            return complet_hand([3, two_pairs[0], two_pairs[1]], cards)
+    elif (straight := is_straight(cards)) != None:
+        return [5, straight]
 
-        elif (one_paire := is_one_pair(cards)) != None:
-            return complet_hand([2, one_paire], cards)
-        
-        else:
-            return complet_hand([1], cards)
+    elif (three_of_kind := is_three_of_kind(cards)) != None:
+        return complet_hand([4, three_of_kind], cards)
+
+    elif (two_pairs := is_two_pairs(cards)) != None:
+        return complet_hand([3, two_pairs[0], two_pairs[1]], cards)
+
+    elif (one_paire := is_one_pair(cards)) != None:
+        return complet_hand([2, one_paire], cards)
+
+    return complet_hand([1], cards)
             
 def better_hand(hand1: list[Card], hand2: list[Card], board: list[Card]) -> bool:
     best_hand1: list = get_best_combination(hand1 + board)
     best_hand2: list = get_best_combination(hand2 + board)
-    
+
     if best_hand1[0] > best_hand2[0]: return True
     elif best_hand1[0] < best_hand2[0]: return False
-    
-    size: int = len(best_hand2) if len(best_hand2) < len(best_hand1) else len(best_hand1)
+
+    size: int = min(len(best_hand2), len(best_hand1))
     for i in range(size):
         if i +1 == size:
             if best_hand1[i] == best_hand2[i]: return False
             elif best_hand1[i] > best_hand2[i]: return True
-    
+
     return False
 
 def count_out(hand: list[Card], board: list[Card]) -> int:
-    out: int = 0    
-    
-    deck :list [Card] = []
+    deck: list [Card] = []
     d: Deck = Deck()
-    
-    for card in d.cards:
-        if not card.exit_in(hand + board):
-            deck.append(card)
-            
-    for card in deck:
-        if better_hand(hand + [card], hand, board): out += 1
-    
+
+    deck.extend(card for card in d.cards if not card.exit_in(hand + board))
+    out: int = sum(bool(better_hand(hand + [card], hand, board)) for card in deck)
     print(out)
     return out
 
@@ -324,4 +313,3 @@ if __name__ == "__main__":
     board: list[Card] = [Card(10,"he art"), Card(7,"diamond"), Card(13,"diamond")]
     
     print(get_proba_out(hand, board, 2))
-        
