f3077c4dbb023c315f53020906bf19978d015ac5
SilverStardust <Yuristeelf@gmail.com>
2023-04-12 08:44:21 +0200

    refactor: :recycle: improve generate_bet_value()
    generate_bet_value() can now return a correct number if the IA doesnt have the necessary bankroll

--- IA.py
+++ IA.py
@@ -11,52 +11,61 @@ from Card import Card
 from Bankroll import Bankroll
 from IA_Proba import *
 
+
 class IA(Player):
-    
+
     def __init__(self: Player, hand: Hand | None = None, bankroll: int = 100) -> None:
         super().__init__("IA", hand, bankroll)
-        
+
     # <----- getter ----->
-    
+
     @property
     def name(self: IA) -> str: return super().name
-    
+
     @property
     def bankroll(self: IA) -> Bankroll: return super().bankroll
-    
+
     @property
     def bet(self: IA) -> int: return super().bet
-    
+
     @property
     def hand(self: IA) -> Hand: return super().hand
-    
+
     @property
     def action(self: IA) -> str: return super().action
-    
+
     # <----- setter ----->
-    
+
     @name.setter
-    def name(self: IA, new_name: str) -> None: object.__getattribute__(self, '_find')(new_name)
-    
+    def name(
+        self: IA, new_name: str) -> None: object.__getattribute__(self, '_find')(new_name)
+
     @bankroll.setter
     def bankroll(self: IA, new_bankroll: Bankroll | int) -> None:
-        if isinstance(new_bankroll, int): object.__getattribute__(self, '_find')(Bankroll(new_bankroll))
-        else: object.__getattribute__(self, '_find')(new_bankroll)
-    
+        if isinstance(new_bankroll, int):
+            object.__getattribute__(self, '_find')(Bankroll(new_bankroll))
+        else:
+            object.__getattribute__(self, '_find')(new_bankroll)
+
     @bet.setter
-    def bet(self: IA, new_bet: int) -> None: object.__getattribute__(self, '_find')(new_bet)
-    
+    def bet(self: IA, new_bet: int) -> None: object.__getattribute__(self,
+                                                                     '_find')(new_bet)
+
     @hand.setter
-    def hand(self: IA, new_hand: Hand) -> None: object.__getattribute__(self, '_find')(new_hand)   
-    
+    def hand(
+        self: IA, new_hand: Hand) -> None: object.__getattribute__(self, '_find')(new_hand)
+
     @action.setter
-    def action(self: IA, new_action: str) -> None: object.__getattribute__(self, '_find')(new_action)
-    
+    def action(
+        self: IA, new_action: str) -> None: object.__getattribute__(self, '_find')(new_action)
+
     # <----- IA decision ----->
-    
+
     def generate_bet_value(self: IA, current_bet: int) -> int:
-        return randint(current_bet, self.bankroll.__value)
-        
+        if current_bet >= self.bankroll.bankroll:
+            return self.bankroll.bankroll
+        return randint(current_bet, self.bankroll.bankroll)
+
     def decision(self: IA, board: list[Card], phase: int, current_dealer: bool, total_blind: int, blind: int, can_check: bool = False) -> tuple[str, int]:
         """Return the action for the IA
 
@@ -72,32 +81,42 @@ class IA(Player):
         proba_out: float = get_proba_out(self.__hand.cards, board, phase)
         hand_power: int = get_hand_power(self.__hand.cards, board)
         pot_odds: float = get_pot_odds(total_blind, blind)
-        
+
         tuple_choice: tuple[str, int] = ('fold', 0)
-        
+
         # if royal flush
         if hand_power == 10:
-            if current_dealer: tuple_choice = ('raise', self.generate_bet_value(blind))
-            else: tuple_choice = choice([('call', 0), ('raise', self.generate_bet_value(blind))])
-        
-        elif proba_out >= 50:
-            if current_dealer: tuple_choice = ('check', self.generate_bet_value(blind))
+            if current_dealer:
+                tuple_choice = ('raise', self.generate_bet_value(blind))
             else:
-                if can_check: tuple_choice= choice([('check', 0), ('raise', self.generate_bet_value(blind))])
-                else: tuple_choice = choice([('call', 0), ('raise', self.generate_bet_value(blind))])
-            
+                tuple_choice = choice(
+                    [('call', 0), ('raise', self.generate_bet_value(blind))])
+
+        elif proba_out >= 50:
+            if current_dealer:
+                tuple_choice = ('check', self.generate_bet_value(blind))
+            else:
+                if can_check:
+                    tuple_choice = choice(
+                        [('check', 0), ('raise', self.generate_bet_value(blind))])
+                else:
+                    tuple_choice = choice(
+                        [('call', 0), ('raise', self.generate_bet_value(blind))])
+
         elif proba_out >= 25:
-            if can_check: tuple_choice = ('check', 0)
-            elif hand_power >= 5: tuple_choice = ('call', 0)
-                
-        self.action = tuple_choice[0]        
+            if can_check:
+                tuple_choice = ('check', 0)
+            elif hand_power >= 5:
+                tuple_choice = ('call', 0)
+
+        self.action = tuple_choice[0]
         return tuple_choice
-        
+
+
 if __name__ == "__main__":
 
     dealer: Dealer = Dealer([Player('IA')], 10)
     dealer.distribute(True)
-    
-    
+
     print(dealer.board)
     print(dealer.players[0].hand)
